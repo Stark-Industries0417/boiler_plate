d8c733e850ab770b8594b2f30cc9c7e843bfc024
Kwon <anstjdrnjs3@gmail.com>
2022-01-17 22:04:52 +0900

    hide mongoDB password

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-
+const config = require('./config/key');
+const {User} = require('./models/User');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://kwon:dbrwl3203!@youtube.sfpas.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected...'))
   .catch(e => console.log(e));
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({success: false, err});
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
