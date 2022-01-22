0a251e4adc3f309eaf582d4b789985f655375804
Kwon <anstjdrnjs3@gmail.com>
2022-01-22 22:16:15 +0900

    Implementation Logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,25 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
 function LandingPage() {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            LandingPage
+        <div style={{
+             display: 'flex', justifyContent: 'center', alignItems: 'center',
+             width: '100%', height: '100vh'}}>
+            <h2>시작 페이지</h2>
+
+            <button onClick={() => axios.get(`/api/users/logout`)
+                                    .then(res => {
+                                        if (res.data.success) {
+                                            navigate('/login');
+                                        } else {
+                                            alert('로그아웃 하는데 실패 하였습니다.');
+                                        }
+                                    })}>
+                logout
+            </button>
         </div>
     )
 }
