1029aa168b553a80a6084c0a06c59e09168233b3
Kwon <anstjdrnjs3@gmail.com>
2022-01-22 23:01:24 +0900

    Wrap the components with Auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
-
+import Auth from './hoc/auth'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path='/register' element={<RegisterPage/>} />
-        <Route path='/' element={<LandingPage />} />
+        <Route path='/login' element={Auth(LoginPage, false)} />
+        <Route path='/register' element={Auth(RegisterPage, false)} />
+        <Route path='/' element={Auth(LandingPage, null)} />
       </Routes>
     </BrowserRouter>
   );
