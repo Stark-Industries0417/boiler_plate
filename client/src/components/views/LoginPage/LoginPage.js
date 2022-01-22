7ca16ada96001ad209bb2ba30f54afe269f42313
Kwon <anstjdrnjs3@gmail.com>
2022-01-22 12:17:18 +0900

    Implement login API useing reducer

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useDispatch} from 'react-redux';
+import{loginUser} from '../../../_actions/user_action';
+import {useNavigate} from 'react-router-dom';
 
-function LoginPage() {
+
+function LoginPage(props) {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onSubmitHandler = e => {
+        e.preventDefault();
+
+        let body = {
+            email,
+            password
+        }
+        dispatch(loginUser(body))
+            .then(res => {
+                if(res.payload.loginSuccess) {
+                    navigate("/");
+                } else {
+                    alert('Error');
+                }
+            })
+    }
+
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <form style={{display: 'flex', flexDirection: 'column'}}
+                  onSubmit={onSubmitHandler}>
+
+                <label>Email</label>
+                <input type='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <label>Password</label>
+                <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+                <br />
+                <button>
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
