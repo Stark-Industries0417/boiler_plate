065f13b5b0d05dbea7766dc1d34ac700bcca326f
Kwon <anstjdrnjs3@gmail.com>
2022-01-22 22:59:34 +0900

    Fixing typing error

--- server/index.js
+++ server/index.js
@@ -65,7 +65,7 @@ app.get('/api/users/auth', auth, (req, res) => {
     res.status(200).json({
         _id: req.user._id,
         isAdmin: req.user.role == 0 ? false : true,
-        isAuth: ture,
+        isAuth: true,
         email: req.user.email,
         name: req.user.name,
         lastname: req.user.lastname,
